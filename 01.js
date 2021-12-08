56626230ebe7c3843159fb81aa2c0310a7356d12
Farhan Rahman <fvzzy@users.noreply.github.com>
2021-12-08 12:51:10 +1100

    Rename variables in day 1 solution

--- 01.js
+++ 01.js
@@ -4,27 +4,27 @@
 */
 
 export function problem1_1(input) {
-  const nums = input.map(Number);
+  const depths = input.map(Number);
 
-  let count = 0;
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] > nums[i - 1]) count++;
+  let increases = 0;
+  for (let i = 1; i < depths.length; i++) {
+    if (depths[i] > depths[i - 1]) increases++;
   }
 
-  return count;
+  return increases;
 }
 
 export function problem1_2(input) {
-  const nums = input.map(Number);
+  const depths = input.map(Number);
 
-  let count = 0;
-  let prevSum = nums[0] + nums[1] + nums[2];
+  let increases = 0;
+  let prevSum = depths[0] + depths[1] + depths[2];
 
-  for (let i = 3; i < nums.length; i++) {
-    let sum = prevSum + nums[i] - nums[i - 3];
-    if (prevSum < sum) count++;
+  for (let i = 3; i < depths.length; i++) {
+    let sum = prevSum + depths[i] - depths[i - 3];
+    if (prevSum < sum) increases++;
     prevSum = sum;
   }
 
-  return count;
+  return increases;
 }
