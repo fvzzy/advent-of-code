cb365c826bf972ece24f448432f45ad9745adbac
Farhan Rahman <fvzzy@users.noreply.github.com>
2021-12-08 15:06:50 +1100

    Add solution for day 4.1

---
+++ 04.js
@@ -0,0 +1,109 @@
+export const day = 4;
+export const title = "giant squid";
+
+const processInput = (input) => {
+  const numbers = input[0].split(",");
+
+  let board = 0;
+  let boards = {};
+
+  for (let line = 1; line < input.length; line++) {
+    // exit if we're at a blank line before the EOF
+    const endOfFile = !input[line] && !input[line + 1];
+    if (endOfFile) break;
+
+    // if we're at the blank line before a new board, initialise an empty board
+    if (!input[line].length) {
+      board += 1;
+      boards[board] = [];
+      continue;
+    }
+
+    // arrange boards represented as matrices, with spaces filtered out
+    boards[board].push(input[line].split(" ").filter((c) => c.length));
+  }
+
+  return { numbers, boards };
+};
+
+const boardTotalSums = (boards) => {
+  let result = {};
+
+  for (let boardIdx in boards) {
+    const board = boards[boardIdx];
+    result[boardIdx] = 0;
+
+    for (let row = 0; row < board.length; row++) {
+      for (let col = 0; col < board[0].length; col++) {
+        result[boardIdx] += Number(board[row][col]);
+      }
+    }
+  }
+
+  return result;
+};
+
+const boardToRowColSets = (board) => {
+  let result = { rows: [], cols: [] };
+
+  for (let row = 0; row < board.length; row++) {
+    result.rows[row] = new Set(board[row]);
+
+    for (let col = 0; col < board[0].length; col++) {
+      const value = board[row][col];
+
+      if (row === 0) {
+        result.cols[col] = new Set([value]);
+      } else {
+        result.cols[col].add(value);
+      }
+    }
+  }
+
+  return result;
+};
+
+const boardsToRowColSets = (boards) => {
+  let result = {};
+
+  for (let boardNumber in boards) {
+    result[boardNumber] = boardToRowColSets(boards[boardNumber]);
+  }
+
+  return result;
+};
+
+export function problem4_1(input) {
+  const { numbers, boards } = processInput(input);
+  const rowsColsSets = boardsToRowColSets(boards);
+  const boardUnmarkedSum = boardTotalSums(boards);
+
+  for (let calledNumber of numbers) {
+    for (let boardNumber in rowsColsSets) {
+      for (let seriesType in rowsColsSets[boardNumber]) {
+        const series = rowsColsSets[boardNumber][seriesType];
+
+        for (let setIdx = 0; setIdx < series.length; setIdx++) {
+          const set = series[setIdx];
+
+          // ignore if this row/column in this board doesn't have the number
+          if (!set.has(calledNumber)) continue;
+
+          // "mark off" a value in its series (row or column)
+          set.delete(calledNumber);
+
+          // calculate the remaining total of all numbers in this board
+          // check if we're on a row set so we only subtract the value once
+          if (seriesType === "rows")
+            boardUnmarkedSum[boardNumber] -= calledNumber;
+
+          // bingo!
+          if (set.size === 0)
+            return calledNumber * boardUnmarkedSum[boardNumber];
+        }
+      }
+    }
+  }
+}
+
+export function problem4_2(input) {}
--- answers.js
+++ answers.js
@@ -2,11 +2,13 @@ import { getInput } from "./lib/utils.js";
 import { problem1_1, problem1_2, title as title1 } from "./01.js";
 import { problem2_1, problem2_2, title as title2 } from "./02.js";
 import { problem3_1, problem3_2, title as title3 } from "./03.js";
+import { problem4_1, problem4_2, title as title4 } from "./04.js";
 
 const solution = {
   1: { problem1: problem1_1, problem2: problem1_2, title: title1 },
   2: { problem1: problem2_1, problem2: problem2_2, title: title2 },
   3: { problem1: problem3_1, problem2: problem3_2, title: title3 },
+  4: { problem1: problem4_1, problem2: problem4_2, title: title4 },
 };
 
 const day = process.argv.slice(2)[0];
