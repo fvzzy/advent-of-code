75bdb31f3315cf90a4d7ecb70bf8ad9763b4f0ab
Farhan Rahman <fvzzy@users.noreply.github.com>
2021-12-09 22:39:39 +1100

    Simplify iterative solution for day 6.1

--- 06.js
+++ 06.js
@@ -3,22 +3,19 @@ export const title = "lanternfish";
 
 const inputToFish = (input) => input[0].split(",").map(Number);
 
-const nextFish = (fish) => {
-  let newFish = 0;
-  for (let i = 0; i < fish.length; i++) {
-    fish[i] === 0 ? (fish[i] = 6) && newFish++ : (fish[i] -= 1);
-  }
-
-  while (newFish > 0) fish.push(8) && newFish--;
-  newFish = 0;
-
-  return fish;
-};
-
 const populationAfterDays = (input, days) => {
-  let fish = inputToFish(input);
-  while (days > 0) (fish = nextFish(fish)) && days--;
-  return fish.length;
+  let stack = inputToFish(input);
+  while (days > 0) {
+    let nextStack = [];
+    while (stack.length) {
+      let fish = stack.shift();
+      const nextFish = fish === 0 ? [6, 8] : [--fish];
+      nextStack.push(...nextFish);
+    }
+    stack = nextStack;
+    days--;
+  }
+  return stack.length;
 };
 
 export function problem6_1(input) {
