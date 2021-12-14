29fa39847e890e5b2599b82260ce0388abcbac32
Farhan Rahman <fvzzy@users.noreply.github.com>
2021-12-14 22:25:43 +1100

    Extract common functions for day 14.2

--- 14.js
+++ 14.js
@@ -1,30 +1,48 @@
 export const day = 14;
 export const title = "extended polymerization";
 
-export function problem14_1(input) {
-  let template = input.shift();
+const instructions = (input) => {
+  const polymer = input[0];
   let rules = {};
-  for (let rule of input) {
-    const [pair, insert] = rule.split(" -> ");
+  for (let line = 2; line < input.length; line++) {
+    const [pair, insert] = input[line].split(" -> ");
     rules[pair] = insert;
   }
 
+  return { polymer, rules };
+};
+
+const maxElementCountDiff = (counts) => {
+  let minFrequency = Infinity;
+  let maxFrequency = -Infinity;
+  for (let element in counts) {
+    minFrequency = Math.min(minFrequency, counts[element]);
+    maxFrequency = Math.max(maxFrequency, counts[element]);
+  }
+
+  return maxFrequency - minFrequency;
+};
+
+export function problem14_1(input) {
+  const { polymer: _polymer, rules } = instructions(input);
+  let polymer = _polymer;
+
   let steps = 10;
   while (steps--) {
-    let nextTemplate = "";
-    for (let i = 0; i < template.length - 1; i++) {
-      nextTemplate += template[i] + rules[`${template[i]}${template[i + 1]}`];
+    let nextPolymer = "";
+    for (let i = 0; i < polymer.length - 1; i++) {
+      nextPolymer += polymer[i] + rules[`${polymer[i]}${polymer[i + 1]}`];
     }
-    template = nextTemplate + template[template.length - 1];
+    polymer = nextPolymer + polymer[polymer.length - 1];
   }
 
   let counts = {};
-  for (let i = 0; i < template.length; i++) {
-    counts[template[i]] ? counts[template[i]]++ : (counts[template[i]] = 1);
+  for (let i = 0; i < polymer.length; i++) {
+    counts[polymer[i]] ? counts[polymer[i]]++ : (counts[polymer[i]] = 1);
   }
 
-  let minFrequency = Infinity;
-  let maxFrequency = -Infinity;
+  return maxElementCountDiff(counts);
+}
   for (let element in counts) {
     minFrequency = Math.min(minFrequency, counts[element]);
     maxFrequency = Math.max(maxFrequency, counts[element]);
