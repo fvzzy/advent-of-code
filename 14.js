b0afbfacb42302d9186fac3b922bc6590dc61bb4
Farhan Rahman <fvzzy@users.noreply.github.com>
2021-12-14 18:10:12 +1100

    Reuse template string as storage for day 14.1

--- 14.js
+++ 14.js
@@ -11,32 +11,23 @@ export function problem14_1(input) {
 
   let steps = 10;
   while (steps--) {
-    let pairs = [];
-    for (let i = 0; i + 1 < template.length; i++) {
-      pairs.push(template[i] + template[i + 1]);
+    let nextTemplate = "";
+    for (let i = 0; i < template.length - 1; i++) {
+      nextTemplate += template[i] + rules[`${template[i]}${template[i + 1]}`];
     }
-
-    let newPairs = [];
-    for (let pair of pairs) {
-      // disregard the last element, as it overlaps with the first element in the next new pair
-      newPairs.push(pair[0] + rules[pair]);
-    }
-    // append on the last element
-    template = newPairs.join("") + template[template.length - 1];
+    template = nextTemplate + template[template.length - 1];
   }
 
-  let elementCounts = {};
+  let counts = {};
   for (let i = 0; i < template.length; i++) {
-    elementCounts[template[i]]
-      ? elementCounts[template[i]]++
-      : (elementCounts[template[i]] = 1);
+    counts[template[i]] ? counts[template[i]]++ : (counts[template[i]] = 1);
   }
 
   let minFrequency = Infinity;
   let maxFrequency = -Infinity;
-  for (let element in elementCounts) {
-    minFrequency = Math.min(minFrequency, elementCounts[element]);
-    maxFrequency = Math.max(maxFrequency, elementCounts[element]);
+  for (let element in counts) {
+    minFrequency = Math.min(minFrequency, counts[element]);
+    maxFrequency = Math.max(maxFrequency, counts[element]);
   }
 
   return maxFrequency - minFrequency;
