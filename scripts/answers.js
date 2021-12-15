dc8bdd83ad96f6a60cf71fe23955cc7d0c62fff2
Farhan Rahman <fvzzy@users.noreply.github.com>
2021-12-16 08:13:50 +1100

    Add performance monitoring to answers

--- scripts/answers.js
+++ scripts/answers.js
@@ -10,8 +10,15 @@ input.pop();
 const { problem1, problem2, title } = solutions[day];
 console.log(`--- day ${day}: ${title} ---`);
 
+const timePart1 = "⚡1";
+const timePart2 = "⚡2";
+
+console.time(timePart1);
 const answer1 = problem1(input);
 console.log("part 1:", answer1);
+console.timeEnd(timePart1);
 
+console.time(timePart2);
 const answer2 = problem2(input);
 console.log("part 2:", answer2);
+console.timeEnd(timePart2);
