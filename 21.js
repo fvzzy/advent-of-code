7e0b2457adb4fe277ac786c4c8750f1683c07852
Farhan Rahman <fvzzy@users.noreply.github.com>
2021-12-21 20:42:03 +1100

    Add a naive inefficient refactor to day 21.2

--- 21.js
+++ 21.js
@@ -32,49 +32,41 @@ export function problem21_1(input) {
 }
 
 export function problem21_2(input) {
+  let possibleDiceTotals = [
+    3, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 8, 8,
+    8, 9,
+  ];
   let p1Wins = 0;
   let p2Wins = 0;
 
-  const takeTurn = (p1, p2, p1Score, p2Score, turn) => {
-    if (p1Score >= 21) {
-      return p1Wins++;
-    }
-    if (p2Score >= 21) {
-      return p2Wins++;
-    }
-    if (turn <= 3) {
-      takeTurn(next(p1, 1), p2, p1Score + next(p1, 1), p2Score, turn + 1);
-      takeTurn(next(p1, 2), p2, p1Score + next(p1, 2), p2Score, turn + 1);
-      takeTurn(next(p1, 3), p2, p1Score + next(p1, 3), p2Score, turn + 1);
+  const takeTurn = (p1, p2, p1Score, p2Score, p1Turn) => {
+    if (p1Score >= 21) return p1Wins++;
+    if (p2Score >= 21) return p2Wins++;
+    if (p1Turn) {
+      for (let roll of possibleDiceTotals) {
+        takeTurn(
+          next(p1, roll),
+          p2,
+          p1Score + next(p1, roll),
+          p2Score,
+          !p1Turn
+        );
+      }
     } else {
-      takeTurn(
-        p1,
-        next(p2, 1),
-        p1Score,
-        p2Score + next(p2, 1),
-        turn >= 6 ? 1 : turn + 1
-      );
-      takeTurn(
-        p1,
-        next(p2, 2),
-        p1Score,
-        p2Score + next(p2, 2),
-        turn >= 6 ? 1 : turn + 1
-      );
-      takeTurn(
-        p1,
-        next(p2, 3),
-        p1Score,
-        p2Score + next(p2, 3),
-        turn >= 6 ? 1 : turn + 1
-      );
+      for (let roll of possibleDiceTotals) {
+        takeTurn(
+          p1,
+          next(p2, roll),
+          p1Score,
+          p2Score + next(p2, roll),
+          !p1Turn
+        );
+      }
     }
   };
 
   let { p1, p2 } = startingPositions(input);
-  takeTurn(p1, p2, 0, 0, 1);
+  takeTurn(p1, p2, 0, 0, true);
 
   return Math.max(p1Wins, p2Wins);
 }
-
-problem21_2(["Player 1 starting position: 4", "Player 2 starting position: 8"]);
