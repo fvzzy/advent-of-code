0752f13ae2b83f540e8323f2846915b62e526a8d
Farhan Rahman <fvzzy@users.noreply.github.com>
2022-12-10 01:28:25 +1100

    Clean up debugging for 2022 day 9.2

--- solutions/2022/09.test.ts
+++ solutions/2022/09.test.ts
@@ -9,51 +9,8 @@ describe(`2022 day 9: "${title2022_9}"`, () => {
       expect(problem2022_9_1(exampleInput)).toBe(13);
     });
 
-    const expectedTailPositions = [
-      "0-0",
-      "1-1",
-      "2-2",
-      "1-3",
-      "2-4",
-      "3-5",
-      "4-5",
-      "5-5",
-      "6-4",
-      "7-3",
-      "8-2",
-      "9-1",
-      "10-0",
-      "9--1",
-      "8--2",
-      "7--3",
-      "6--4",
-      "5--5",
-      "4--5",
-      "3--5",
-      "2--5",
-      "1--5",
-      "0--5",
-      "-1--5",
-      "-2--5",
-      "-3--4",
-      "-4--3",
-      "-5--2",
-      "-6--1",
-      "-7-0",
-      "-8-1",
-      "-9-2",
-      "-10-3",
-      "-11-4",
-      "-11-5",
-      "-11-6",
-    ];
-
-    const expected = new Set([...expectedTailPositions]);
-
     test("part 2 solution works", () => {
       expect(problem2022_9_2(exampleInput)).toBe(1);
-      // expect(expected.size).toBe(36);
-      // expect(problem2022_9_2(exampleInput2)).toBe(expected);
       expect(problem2022_9_2(exampleInput2)).toBe(36);
     });
   });
--- solutions/2022/09.ts
+++ solutions/2022/09.ts
@@ -1,25 +1,6 @@
 // https://adventofcode.com/2022/day/9
 export const title2022_9 = "rope bridge";
 
-// well fuck it I guess we're printing the whole thing to debug this
-function createGrid(width: number, height: number) {
-  const grid = [];
-  for (let y = 0; y < height; y++) {
-    const row = [];
-    for (let x = 0; x < width; x++) {
-      row.push(" . ");
-    }
-    grid.push(row);
-  }
-  return grid;
-}
-
-function printGrid(grid: [][]): void {
-  for (let row = grid.length - 1; row >= 0; row--) {
-    console.log(grid[row].join(""));
-  }
-}
-
 const MOVES = { U: [0, 1], D: [0, -1], L: [-1, 0], R: [1, 0] };
 
 export function problem2022_9_1(input: string[]) {
@@ -45,36 +26,19 @@ export function problem2022_9_1(input: string[]) {
 }
 
 export function problem2022_9_2(input: string[]) {
-  //   const exampleInput2 = ["R 5", "U 8", "L 8"];
-  let startX = 11;
-  let startY = 5;
-
   const knots = [...Array(10)].map(() => [0, 0]);
   const tailPositions = new Set();
 
   for (let headMove of input) {
-    // const grid = createGrid(26, 21);
-
     const [headMoveDir, headMoveSteps] = headMove.split(" ");
 
     for (let step = Number(headMoveSteps); step > 0; step--) {
-      // move head
-      //   grid[knots[0][1] + startY][knots[0][0] + startX] = ` . `;
-
       knots[0][0] += MOVES[headMoveDir][0];
       knots[0][1] += MOVES[headMoveDir][1];
 
-      //   let lastHeadMove = MOVES[headMoveDir];
-
-      //   grid[knots[0][1] + startY][knots[0][0] + startX] = ` H `;
-
-      // move body
       for (let i = 1; i < knots.length; i++) {
         const currHead = knots[i - 1];
         const curr = knots[i];
-        // const [prevX, prevY] = knots[i];
-
-        // grid[knots[i][1] + startY][knots[i][0] + startX] = ` . `;
 
         if (Math.abs(currHead[0] - curr[0]) >= 2 || Math.abs(currHead[1] - curr[1]) >= 2) {
           if (curr[0] !== currHead[0]) {
@@ -85,16 +49,10 @@ export function problem2022_9_2(input: string[]) {
           }
         }
 
-        // grid[startY][startX] = "s";
-        // grid[knots[i][1] + startY][knots[i][0] + startX] = ` ${i} `;
-        // console.log(`direction: ${headMoveDir}, step: ${Number(headMoveSteps) - step}`);
-        // printGrid(grid);
-
         if (i === 9) tailPositions.add(`${knots[i][0]}-${knots[i][1]}`);
       }
     }
   }
 
-  //   return tailPositions.size;
   return tailPositions.size;
 }
