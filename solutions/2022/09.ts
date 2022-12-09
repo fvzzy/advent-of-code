dcc2831b88400d58f5face0ca56bf9a5b32fe2fc
Farhan Rahman <fvzzy@users.noreply.github.com>
2022-12-10 01:25:13 +1100

    Add working solution for 2022 day 9.2

--- solutions/2022/09.ts
+++ solutions/2022/09.ts
@@ -2,7 +2,7 @@
 export const title2022_9 = "rope bridge";
 
 // well fuck it I guess we're printing the whole thing to debug this
-function createGrid(width: number, height: number): [][] {
+function createGrid(width: number, height: number) {
   const grid = [];
   for (let y = 0; y < height; y++) {
     const row = [];
@@ -45,71 +45,56 @@ export function problem2022_9_1(input: string[]) {
 }
 
 export function problem2022_9_2(input: string[]) {
-  const exampleInput2 = ["R 5", "U 8", "L 8"];
+  //   const exampleInput2 = ["R 5", "U 8", "L 8"];
   let startX = 11;
   let startY = 5;
 
   const knots = [...Array(10)].map(() => [0, 0]);
   const tailPositions = new Set();
 
-  for (let headMove of exampleInput2) {
-    const grid = createGrid(26, 21);
+  for (let headMove of input) {
+    // const grid = createGrid(26, 21);
 
     const [headMoveDir, headMoveSteps] = headMove.split(" ");
 
     for (let step = Number(headMoveSteps); step > 0; step--) {
       // move head
-      grid[knots[0][1] + startY][knots[0][0] + startX] = ` . `;
+      //   grid[knots[0][1] + startY][knots[0][0] + startX] = ` . `;
 
       knots[0][0] += MOVES[headMoveDir][0];
       knots[0][1] += MOVES[headMoveDir][1];
 
-      let lastHeadMove = MOVES[headMoveDir];
+      //   let lastHeadMove = MOVES[headMoveDir];
 
-      grid[knots[0][1] + startY][knots[0][0] + startX] = ` H `;
+      //   grid[knots[0][1] + startY][knots[0][0] + startX] = ` H `;
 
       // move body
       for (let i = 1; i < knots.length; i++) {
         const currHead = knots[i - 1];
         const curr = knots[i];
-        const [prevX, prevY] = knots[i];
+        // const [prevX, prevY] = knots[i];
 
-        grid[knots[i][1] + startY][knots[i][0] + startX] = ` . `;
-
-        console.log(`moving ${i} based on last head move ${lastHeadMove} for ${i - 1}`);
+        // grid[knots[i][1] + startY][knots[i][0] + startX] = ` . `;
 
         if (Math.abs(currHead[0] - curr[0]) >= 2 || Math.abs(currHead[1] - curr[1]) >= 2) {
-          if (lastHeadMove[0] && lastHeadMove[1]) {
-            // diagonal move
-            knots[i][0] += lastHeadMove[0];
-            knots[i][1] += lastHeadMove[1];
-          } else if (lastHeadMove[0]) {
-            // horizontal move
-            knots[i][0] += lastHeadMove[0];
-
-            if (currHead[1] !== curr[1]) {
-              knots[i][1] = currHead[1];
-            }
-          } else if (lastHeadMove[1]) {
-            // vertical move
-            knots[i][1] += lastHeadMove[1];
-
-            if (currHead[0] !== curr[0]) {
-              knots[i][0] = currHead[0];
-            }
+          if (curr[0] !== currHead[0]) {
+            knots[i][0] = currHead[0] > curr[0] ? curr[0] + 1 : curr[0] - 1;
           }
-          lastHeadMove = [knots[i][0] - prevX, knots[i][1] - prevY];
+          if (curr[1] !== currHead[1]) {
+            knots[i][1] = currHead[1] > curr[1] ? curr[1] + 1 : curr[1] - 1;
+          }
         }
 
-        grid[startY][startX] = "s";
-        grid[knots[i][1] + startY][knots[i][0] + startX] = ` ${i} `;
-        console.log(`direction: ${headMoveDir}, step: ${Number(headMoveSteps) - step}`);
-        printGrid(grid);
+        // grid[startY][startX] = "s";
+        // grid[knots[i][1] + startY][knots[i][0] + startX] = ` ${i} `;
+        // console.log(`direction: ${headMoveDir}, step: ${Number(headMoveSteps) - step}`);
+        // printGrid(grid);
 
         if (i === 9) tailPositions.add(`${knots[i][0]}-${knots[i][1]}`);
       }
     }
   }
 
+  //   return tailPositions.size;
   return tailPositions.size;
 }
