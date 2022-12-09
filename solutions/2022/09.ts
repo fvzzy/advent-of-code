02c50dbc79204de3868afc85f51b4c8131084b72
Farhan Rahman <fvzzy@users.noreply.github.com>
2022-12-09 17:02:41 +1100

    Fix solution for 2022 day 9.1

--- solutions/2022/09.ts
+++ solutions/2022/09.ts
@@ -2,21 +2,28 @@
 export const title2022_9 = "rope bridge";
 
 export function problem2022_9_1(input: string[]) {
-  let h = [0, 0];
-  //   let t = [0, 0]; // don't think this is required yet?
-  const tPositions = new Set();
-  const moves = {
-    U: [0, 1],
-    D: [0, -1],
-    L: [-1, 0],
-    R: [1, 0],
-  };
+  const MOVES = { U: [0, 1], D: [0, -1], L: [-1, 0], R: [1, 0] };
 
-  for (let hMove of input) {
-    const [dir, distance] = hMove.split(" ");
-    for (let step = Number(distance); step > 0; step--) {
-      h = [h[0] + moves[dir][0], h[1] + moves[dir][1]];
-      if (step !== 1) tPositions.add(`${h[0]}-${h[1]}`);
+  let h = [0, 0];
+  let tX = h[0];
+  let tY = h[1];
+  const tPositions = new Set();
+
+  for (let line = 0; line < input.length; line++) {
+    const move = input[line];
+    const [dir, steps] = move.split(" ");
+
+    for (let step = Number(steps); step > 0; step--) {
+      const hX = h[0] + MOVES[dir][0];
+      const hY = h[1] + MOVES[dir][1];
+
+      h = [hX, hY];
+      if (Math.abs(hX - tX) >= 2 || Math.abs(hY - tY) >= 2) {
+        tX = hX - MOVES[dir][0];
+        tY = hY - MOVES[dir][1];
+      }
+
+      tPositions.add(`${tX}-${tY}`);
     }
   }
 
