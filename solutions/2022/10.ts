074d7b5b9d98dd30138d50f10e4d09215aa18641
Farhan Rahman <fvzzy@users.noreply.github.com>
2022-12-10 18:13:24 +1100

    Slightly clean up 2022 day 10 solutions

--- solutions/2022/10.ts
+++ solutions/2022/10.ts
@@ -8,16 +8,18 @@ export function problem2022_10_1(input: string[]) {
   let x = 1;
   let cycle = 1;
 
-  for (let instruction of input) {
-    const arg = instruction.split(" ")[1];
-
+  const updateSignalStrength = () => {
     cycle += 1;
     if (SIGNIFICANT_CYCLES.includes(cycle)) signalStrengthTotal += cycle * x;
+  };
+
+  for (let instruction of input) {
+    const arg = instruction.split(" ")[1];
+    updateSignalStrength();
 
     if (arg) {
-      cycle += 1;
       x += Number(arg);
-      if (SIGNIFICANT_CYCLES.includes(cycle)) signalStrengthTotal += cycle * x;
+      updateSignalStrength();
     }
   }
 
@@ -28,28 +30,24 @@ export function problem2022_10_2(input: string[]) {
   const CRT_WIDTH = 40;
   const CRT_HEIGHT = 6;
 
-  let linearRegister = Array(CRT_HEIGHT * CRT_WIDTH).fill(".");
-  let x = 0;
+  const linearRegister = Array(CRT_HEIGHT * CRT_WIDTH).fill(".");
+  let x = 1;
   let cycle = 0;
+
+  const drawPixel = () => {
+    if ([x - 1, x, x + 1].includes(cycle % 40)) linearRegister[cycle] = "#";
+    cycle += 1;
+  };
 
   for (let instruction of input) {
     const arg = instruction.split(" ")[1];
-
-    const offset = CRT_WIDTH * Math.floor((cycle + 1) / CRT_WIDTH);
-    const spritePos = [1, 2, 3].map((regPos) => regPos + x + offset);
-
-    cycle += 1;
-    if (spritePos.includes(cycle)) linearRegister[cycle] = "#";
+    drawPixel();
 
     if (arg) {
-      cycle += 1;
-      if (spritePos.includes(cycle)) linearRegister[cycle] = "#";
+      drawPixel();
       x += Number(arg);
     }
   }
-
-  linearRegister.shift(); // off by one somewhere
-  linearRegister.push("."); // hacks to get around a half-baked solution
 
   const output = [];
   for (let i = 0; i < linearRegister.length; i += CRT_WIDTH) {
