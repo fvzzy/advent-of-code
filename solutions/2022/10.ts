333a9e106778f21401646e74ce287e4c6277c1a8
Farhan Rahman <fvzzy@users.noreply.github.com>
2022-12-10 17:28:32 +1100

    Add incomplete but usable solution for 2022 day 10.2

--- solutions/2022/10.ts
+++ solutions/2022/10.ts
@@ -24,4 +24,37 @@ export function problem2022_10_1(input: string[]) {
   return signalStrengthTotal;
 }
 
-export function problem2022_10_2(input: string[]) {}
+export function problem2022_10_2(input: string[]) {
+  const CRT_WIDTH = 40;
+  const CRT_HEIGHT = 6;
+
+  let linearRegister = Array(CRT_HEIGHT * CRT_WIDTH).fill(".");
+  let x = 0;
+  let cycle = 0;
+
+  for (let instruction of input) {
+    const arg = instruction.split(" ")[1];
+
+    const offset = CRT_WIDTH * Math.floor((cycle + 1) / CRT_WIDTH);
+    const spritePos = [1, 2, 3].map((regPos) => regPos + x + offset);
+
+    cycle += 1;
+    if (spritePos.includes(cycle)) linearRegister[cycle] = "#";
+
+    if (arg) {
+      cycle += 1;
+      if (spritePos.includes(cycle)) linearRegister[cycle] = "#";
+      x += Number(arg);
+    }
+  }
+
+  linearRegister.shift(); // off by one somewhere
+  linearRegister.push("."); // hacks to get around a half-baked solution
+
+  const output = [];
+  for (let i = 0; i < linearRegister.length; i += CRT_WIDTH) {
+    output.push(linearRegister.slice(i, i + CRT_WIDTH).join(""));
+  }
+
+  return output;
+}
