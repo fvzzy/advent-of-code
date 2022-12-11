780bb6688ec4810b9899c0fd5bec04d910d6d200
Farhan Rahman <fvzzy@users.noreply.github.com>
2022-12-11 17:30:12 +1100

    Add solution for 2022 day 11.1

--- solutions/2022/11.ts
+++ solutions/2022/11.ts
@@ -2,10 +2,8 @@
 export const title2022_11 = "monkey in the middle";
 
 export function problem2022_11_1(input: string[]) {
-  const monkeys = [];
-  let monkeyBusiness = 0;
-
   // organise inputs into something more usable
+  const monkeys = []; // TODO: type this
   let i = 0;
   while (i < input.length) {
     const monkey = Number(input[i++].slice(0, -1).split(" ")[1]);
@@ -19,11 +17,27 @@ export function problem2022_11_1(input: string[]) {
     const ifFalse = Number(input[i++].replace("If false: throw to monkey ", ""));
     const test = new Function("worry", `return worry % ${divisor} === 0 ? ${ifTrue} : ${ifFalse}`);
 
-    monkeys[monkey] = { items, operation, test };
+    monkeys[monkey] = { items, operation, test, inspections: 0 };
     i++;
   }
 
-  return monkeyBusiness;
+  for (let round = 20; round > 0; round--) {
+    for (let i = 0; i < monkeys.length; i++) {
+      const { items, operation, test } = monkeys[i];
+      monkeys[i].inspections += items.length;
+
+      while (items.length) {
+        const item = items.shift();
+        const worry = Math.floor(operation(item) / 3);
+        const receipient = test(worry);
+        monkeys[receipient].items.push(worry);
+      }
+    }
+  }
+
+  const sortedInspections = monkeys.map((monkey) => monkey.inspections).sort((a, b) => b - a);
+
+  return sortedInspections[0] * sortedInspections[1];
 }
 
 export function problem2022_11_2(input: string[]) {}
