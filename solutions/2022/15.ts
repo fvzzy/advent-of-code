b0b34404575bc1571a8483136ee0182946fe9f92
Farhan Rahman <fvzzy@users.noreply.github.com>
2022-12-16 09:46:21 +1100

    Only check target row for 2022 day 15.1

--- solutions/2022/15.ts
+++ solutions/2022/15.ts
@@ -6,38 +6,29 @@ function manhattanDistance(p1x, p1y, p2x, p2y) {
 }
 
 export function problem2022_15_1(input: string[]) {
-  // const reports = input.map((line) => line.match(/-?\d+/g).map(Number));
-  const reports = [[8, 7, 2, 10]]; // example from problem statement
-  const coveredPoints = new Set();
-  // debugging grid
-  // const xVals = reports.map((report) => [report[0], report[2]]).flat();
-  // const yVals = reports.map((report) => [report[1], report[3]]).flat();
-  // const [minX, minY, maxX, maxY] = [Math.min(...xVals), Math.min(...yVals), Math.max(...xVals), Math.max(...yVals)];
-  // const grid = [...Array(maxY - minY + 1)].map(() => [...Array(maxX - minX + 1)].map(() => " "));
-  // for (let [sX, sY, bX, bY] of reports) {
-  //   grid[sY - minY][sX - minX] = "S";
-  //   grid[bY - minY][bX - minX] = "B";
-  // }
-  // console.table(grid);
+  const targetRow = 2000000;
+  const reports = input.map((line) => line.match(/-?\d+/g).map(Number));
+  const coveredPoints: Set<string> = new Set();
+
+  const beaconsXInTargetRow = new Set([
+    ...reports
+      .map((r) => r.slice(-2))
+      .filter((b) => b[1] === targetRow)
+      .map((b) => b[0]),
+  ]);
 
   for (let [sX, sY, bX, bY] of reports) {
-    coveredPoints.add(`${sX}_${sY}`);
-    coveredPoints.add(`${bX}_${bY}`);
+    if (sY === targetRow) coveredPoints.add(`${sX}_${sY}`);
     const maxDistance = manhattanDistance(sX, sY, bX, bY);
-    // console.log(distance);
 
-    // draw a bounding box around sensor for all possible correct distances
     for (let x = sX - maxDistance; x <= sX + maxDistance; x++) {
-      for (let y = sY - maxDistance; y <= sY + maxDistance; y++) {
-        // console.log(x, y);
-        if (maxDistance >= manhattanDistance(sX, sY, x, y)) {
-          coveredPoints.add(`${x}_${y}`);
-        }
+      if (maxDistance >= manhattanDistance(sX, sY, x, targetRow) && !beaconsXInTargetRow.has(x)) {
+        coveredPoints.add(`${x}_${targetRow}`);
       }
     }
   }
 
-  console.log(coveredPoints);
+  return coveredPoints.size;
 }
 
 export function problem2022_15_2(input: string[]) {}
