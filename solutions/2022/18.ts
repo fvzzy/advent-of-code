1139276616ea00f3d71f6fd124f85fa2dd92667e
Farhan Rahman <fvzzy@users.noreply.github.com>
2022-12-18 21:27:21 +1100

    Add basic solution for 2022 day 18.1

--- solutions/2022/18.ts
+++ solutions/2022/18.ts
@@ -2,16 +2,21 @@
 export const title2022_18 = "boiling boulders";
 
 export function problem2022_18_1(input: string[]) {
-  const cubes = input.map((cube) => cube.split(",").map(Number));
-  const planeXY = new Set();
-  const planeYZ = new Set();
-  const planeXZ = new Set();
+  const cubesArr: number[][] = input.map((cube) => cube.split(",").map(Number));
+  const cubes: Set<string> = new Set();
   let surfaceArea = 0;
 
-  for (let [x, y, z] of cubes) {
-    planeXY.add(`${x}-${y}`);
-    planeYZ.add(`${y}-${z}`);
-    planeXZ.add(`${x}-${z}`);
+  for (let [x, y, z] of cubesArr) {
+    cubes.add(`${x}-${y}-${z}`);
+  }
+
+  for (let [x, y, z] of cubesArr) {
+    if (!cubes.has(`${x + 1}-${y}-${z}`)) surfaceArea++;
+    if (!cubes.has(`${x - 1}-${y}-${z}`)) surfaceArea++;
+    if (!cubes.has(`${x}-${y + 1}-${z}`)) surfaceArea++;
+    if (!cubes.has(`${x}-${y - 1}-${z}`)) surfaceArea++;
+    if (!cubes.has(`${x}-${y}-${z + 1}`)) surfaceArea++;
+    if (!cubes.has(`${x}-${y}-${z - 1}`)) surfaceArea++;
   }
 
   return surfaceArea;
