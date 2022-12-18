f40034b34f7b07c3a323a68cde12bb0ce47eb894
Farhan Rahman <fvzzy@users.noreply.github.com>
2022-12-19 09:44:15 +1100

    Add brute-force solution for 2022 day 15.2

--- solutions/2022/15.ts
+++ solutions/2022/15.ts
@@ -1,14 +1,14 @@
 // https://adventofcode.com/2022/day/15
 export const title2022_15 = "beacon exclusion zone";
 
-function manhattanDistance(p1x, p1y, p2x, p2y) {
+function manhattanDistance(p1x: number, p1y: number, p2x: number, p2y: number) {
   return Math.abs(p1x - p2x) + Math.abs(p1y - p2y);
 }
 
 export function problem2022_15_1(input: string[]) {
-  const target = 10;
+  const target = input.length === 14 ? 10 : 2000000;
   const reports = input.map((line) => line.match(/-?\d+/g).map(Number));
-  const coveredPoints: Set<string> = new Set();
+  const coveredPoints: Set<number> = new Set();
 
   const beaconsXAtTarget = new Set([
     ...reports
@@ -18,12 +18,13 @@ export function problem2022_15_1(input: string[]) {
   ]);
 
   for (let [sX, sY, bX, bY] of reports) {
-    if (sY === target) coveredPoints.add(`${sX}_${sY}`);
+    if (sY === target) coveredPoints.add(sX); // add sensor to covered points since a beacon can't also exist there
     const distance = manhattanDistance(sX, sY, bX, bY);
 
     for (let x = sX - distance; x <= sX + distance; x++) {
+      // iterate through points that could fall in the sensor's reach
       if (distance >= manhattanDistance(sX, sY, x, target) && !beaconsXAtTarget.has(x)) {
-        coveredPoints.add(`${x}_${target}`);
+        coveredPoints.add(x);
       }
     }
   }
@@ -32,27 +33,35 @@ export function problem2022_15_1(input: string[]) {
 }
 
 export function problem2022_15_2(input: string[]) {
-  // const upperBound = 4000000;
+  const maxXY = input.length === 14 ? 20 : 4000000;
   const reports = input.map((line) => line.match(/-?\d+/g).map(Number));
   const beacons = new Set([...reports.map((r) => r.slice(-2).join("_"))]);
   const coveredPoints: Set<string> = new Set();
+  let foundBeacon: number[];
 
-  let target = 3; // need to loop over each row after this returns the correct size...
-  for (let [sX, sY, bX, bY] of reports) {
-    if (sY === target) coveredPoints.add(`${sX}_${sY}`);
-    const distance = manhattanDistance(sX, sY, bX, bY);
-
-    // for (let x = sX - distance; x <= sX + distance && x >= 0 && x <= 20; x++) {
-    for (let x = 0; x <= 20; x++) {
-      if (distance >= manhattanDistance(sX, sY, x, target) && !beacons.has(`${x}_${target}`)) {
-        coveredPoints.add(`${x}_${target}`);
+  for (let y = 0; y <= maxXY; y++) {
+    for (let [sX, sY, bX, bY] of reports) {
+      if (sY === y) coveredPoints.add(`${sX}`);
+      const distance = manhattanDistance(sX, sY, bX, bY);
+      for (let x = Math.max(sX - distance, 0); x <= Math.min(sX + distance, maxXY); x++) {
+        if (distance >= manhattanDistance(sX, sY, x, y) && !beacons.has(`${x}`)) {
+          coveredPoints.add(`${x}`);
+        }
       }
     }
+
+    if (coveredPoints.size !== maxXY + 1) {
+      for (let i = 0; i <= maxXY; i++) {
+        if (!coveredPoints.has(`${i}`)) {
+          foundBeacon = [i, y];
+          break;
+        }
+      }
+      break;
+    }
+
+    coveredPoints.clear();
   }
 
-  console.log(coveredPoints);
-  console.log(coveredPoints.size);
-
-  // const foundBeacon = [14, 11];
-  // return foundBeacon[0] * upperBound + foundBeacon[1];
+  return foundBeacon[0] * 4000000 + foundBeacon[1];
 }
