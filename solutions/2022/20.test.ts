b45b6604bb8edce0df87d0c8cf81be7e8b023f70
Farhan Rahman <fvzzy@users.noreply.github.com>
2022-12-20 23:28:38 +1100

    Amend approach and add solution for 2022 day 20.1

--- solutions/2022/20.test.ts
+++ solutions/2022/20.test.ts
@@ -1,37 +1,7 @@
-import { problem2022_20_1, problem2022_20_2, title2022_20, cycle, mixOne, mix } from "./20";
+import { problem2022_20_1, problem2022_20_2, title2022_20 } from "./20";
 
 describe(`2022 day 20: "${title2022_20}"`, () => {
-  describe("#cycle", () => {
-    test("returns the correct index when moving forwards", () => {
-      expect(cycle(2, 12, 10)).toBe(4);
-      expect(cycle(4, 1000, 7)).toBe(3);
-      expect(cycle(4, 2000, 7)).toBe(2);
-      expect(cycle(4, 3000, 7)).toBe(1);
-    });
-
-    test("returns the correct index when moving backwards", () => {
-      expect(cycle(1, -3, 10)).toBe(8);
-    });
-  });
-
-  describe("#mixOne", () => {
-    test("shuffles numbers forward by the correct amount", () => {
-      expect(mixOne(0, [1, 2, -3, 3, -2, 0, 4])).toStrictEqual([2, 1, -3, 3, -2, 0, 4]);
-      expect(mixOne(0, [2, 1, -3, 3, -2, 0, 4])).toStrictEqual([1, -3, 2, 3, -2, 0, 4]);
-      expect(mixOne(2, [1, 2, 3, -2, -3, 0, 4])).toStrictEqual([1, 2, -2, -3, 0, 3, 4]);
-    });
-
-    test("wraps numbers around when moving past the end of the list", () => {
-      expect(mixOne(5, [1, 2, -3, 0, 3, 4, -2])).toStrictEqual([1, 2, -3, 4, 0, 3, -2]);
-      expect(mixOne(1, [1, -3, 2, 3, -2, 0, 4])).toStrictEqual([1, 2, 3, -2, -3, 0, 4]);
-      expect(mixOne(2, [1, 2, -2, -3, 0, 3, 4])).toStrictEqual([1, 2, -3, 0, 3, 4, -2]);
-      expect(mixOne(4, [1, 5, -2, -3, 2, 3, 4])).toStrictEqual([1, 5, -2, -3, 3, 4, 2]);
-    });
-  });
-
-  describe("#mix", () => {});
-
-  describe.skip("example inputs", () => {
+  describe("example inputs", () => {
     const exampleInput = ["1", "2", "-3", "3", "-2", "0", "4"];
 
     test("part 1 solution works", () => {
--- solutions/2022/20.ts
+++ solutions/2022/20.ts
@@ -1,46 +1,143 @@
 // https://adventofcode.com/2022/day/20
 export const title2022_20 = "grove positioning system";
 
-export function cycle(idx: number, move: number, arrLength: number) {
-  if (move === 0) return idx;
-  else if (move > 0) return (idx + move) % arrLength;
-  else return (idx + move) % arrLength;
+class Node {
+  value: number;
+  addOrder: number;
+  next: Node;
+  prev: Node;
+
+  constructor(value: number) {
+    this.value = value;
+    this.addOrder = null;
+    this.next = null;
+    this.prev = null;
+  }
+
+  unlink() {
+    this.prev.next = this.next;
+    this.next.prev = this.prev;
+
+    return this;
+  }
 }
 
-export function mixOne(idx: number, numbers: number[]): number[] {
-  const mixed = [...numbers];
-  const cut = mixed.splice(idx, 1)[0];
+class CyclicalList {
+  size: number;
+  cursor: Node;
 
-  if (cut == 0) return mixed;
-  let newIdx: number;
+  constructor(values = []) {
+    this.size = 0;
+    this.cursor = null;
 
-  if (cut > 0) {
-    if (cut + idx > numbers.length - 1) {
-      newIdx = (cut + idx + 1) % numbers.length;
-    } else {
-      newIdx = (cut + idx) % numbers.length;
-    }
-  } else if (cut < 0) {
-    if (cut + idx === 0) {
-      newIdx = numbers.length - 1;
-    } else if (cut + idx < 0) {
-      newIdx = (cut + idx) % numbers.length;
-    } else {
-      newIdx = (cut + idx) % numbers.length;
+    for (let value of values) {
+      this.add(value);
     }
   }
 
-  mixed.splice(newIdx, 0, cut);
-  return mixed;
-}
+  add(value: number) {
+    const node = new Node(value);
 
-export function mix(numbers: number[]): number[] {
-  const mixedNumbers = [...numbers];
-  return mixedNumbers;
+    this.size++;
+    node.addOrder = this.size;
+
+    if (!this.cursor) {
+      node.next = node.prev = this.cursor = node;
+    } else {
+      node.next = this.cursor;
+      node.prev = this.cursor.prev;
+      node.prev.next = node;
+      this.cursor.prev = node;
+    }
+  }
+
+  insert(node: Node) {
+    if (!this.cursor) return undefined;
+    this.size++;
+
+    node.next = this.cursor.next;
+    this.cursor.next = node;
+    node.prev = this.cursor;
+    node.next.prev = node;
+
+    return this;
+  }
+
+  remove() {
+    if (!this.cursor) return undefined;
+    this.size--;
+    if (this.size === 0) {
+      this.cursor = null;
+      return null;
+    } else {
+      const node = this.cursor;
+      this.cursor = this.cursor.next;
+      return node.unlink();
+    }
+  }
+
+  move(steps: number) {
+    const node = this.remove();
+    return this.rotate(steps - 1).insert(node); // TODO: remove the -1 to make this a bit more obvious
+  }
+
+  mix() {
+    let mixed = 1;
+
+    while (mixed <= this.size) {
+      while (this.cursor.addOrder !== mixed) {
+        this.cursor = this.cursor.next;
+      }
+      this.move(this.cursor.value);
+      mixed++;
+    }
+
+    return this;
+  }
+
+  rotate(steps: number) {
+    let offset = steps % this.size;
+
+    if (offset > 0) {
+      while (offset--) this.cursor = this.cursor.next;
+    } else if (offset < 0) {
+      while (offset++) this.cursor = this.cursor.prev;
+    }
+
+    return this;
+  }
+
+  zero() {
+    while (this.cursor.value !== 0) {
+      this.cursor = this.cursor.next;
+    }
+
+    return this;
+  }
+
+  toArray() {
+    const items = [];
+    if (!this.size) return items;
+
+    let node = this.cursor;
+    for (let i = 0; i < this.size; i++) {
+      items.push(node.value);
+      node = node.next;
+    }
+
+    return items;
+  }
 }
 
 export function problem2022_20_1(input: string[]) {
-  const numbers = input.map(Number);
+  const fileNumbers = input.map(Number);
+  const list = new CyclicalList(fileNumbers);
+
+  const coord1 = list.mix().zero().rotate(1000).cursor.value;
+  const coord2 = list.zero().rotate(2000).cursor.value;
+  const coord3 = list.zero().rotate(3000).cursor.value;
+
+  return coord1 + coord2 + coord3;
 }
 
 export function problem2022_20_2(input: string[]) {}
